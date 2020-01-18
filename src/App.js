98af60b0ff9eafe852c4c4dc4e030111b1ef5464
Matthew Hoang <hoangmatthew1999@gmail.com>
2020-01-17 23:49:41 -0800

    starting getting the cards done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {Navbar,Nav} from 'react-bootstrap';
+import {Navbar,Nav, ListGroup, Card, Button} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Navigation(){
   return(
     <Navbar bg="primary" variant="dark">
-    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+    <Navbar.Brand href="#home"></Navbar.Brand>
     <Nav className="mr-auto">
       <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#features">Features</Nav.Link>
-      <Nav.Link href="#pricing">Pricing</Nav.Link>
+      <Nav.Link href="#forum">Forum</Nav.Link>
+      <Nav.Link href="#talk-to-professionals">Talk to professionals</Nav.Link>
     </Nav>
   </Navbar>
+  )
+}
+function CardBody(){
+  return (
+    <Card.Body>
+        <Card.Title>Special title treatment</Card.Title>
+        <Card.Text>
+          With supporting text below as a natural lead-in to additional content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+  )
+}
+function MajorDiv(){
+  return(
+    <Card>
+      <Card.Header>Featured</Card.Header>
+
+      <ListGroup>
+      <ListGroup.Item><CardBody/></ListGroup.Item>
+      <ListGroup.Item><CardBody/></ListGroup.Item>
+      </ListGroup>
+  </Card>
+  )
+}
+function BigCard(){
+  return(
+  <div>
+    <ListGroup.Item><MajorDiv/></ListGroup.Item>
+    <ListGroup.Item><MajorDiv/></ListGroup.Item>
+    <ListGroup.Item><MajorDiv/></ListGroup.Item>
+  </div>
   )
 }
 function App() {
   return (
     <div className="App">
      <Navigation/>
+     <BigCard/>
     </div>
   );
 }
